c6df1af35c9421085c93f32b233d84a7158cef72
Christian Pfitzner <christian.pfitzner@evocortex.com>
2022-05-17 16:02:53 +0200

    Update kalman_intro_template.m

--- 01_introduction/kalman_intro_template.m
+++ 01_introduction/kalman_intro_template.m
@@ -1,20 +1,20 @@
 clear all;
 close all; 
 
-% Anzahl Messwerte
+% number of values in the measurement vector
 vals    = 100;
 
-% Streuung der Messwerte
+% noise in sensor data for gps
 sigma = 5.0;
 
-% ZustandsÃ¼bergangsmodel
+% description of system 
 dT = 1.0;
 A = [1    0    dT   0;
 	0    1    0     dT;
 	0    0    1     0;
 	0    0    0     1];
 
-% Realer Zustand
+% ground truth state
 x = [0 0 1 1]';
 
 % Messwerte generieren
@@ -27,68 +27,51 @@ x = [0 0 1 1]';
 % Nur die Position ist messbar, nicht die Geschwindigkeit
 H = [1 0 0 0; 0 1 0 0];
 
-% Messrauschen
+% measurement noise 
 R = [sigma 0; 
     0 sigma];
 
-% Systemrauschen
+% system noise
 q = 0.001;
 Q = [0 0 0 0;
      0 0 0 0; 
      0 0 0 0; 
      0 0 0 0];
 
-% Kovarianz
+% covariance matrix
 P = [1 0 0 0;
      0 1 0 0; 
      0 0 1 0; 
      0 0 0 1];
 
-P_data_for_plot = zeros(vals, 4);
-K_data_for_plot = zeros(vals, 2); 
+
      
-% Implementieren Sie hier den Kalmanfilter
+% TASK 1: Impement the Kalman Filter in the upcomming loop
 for i=1:vals 
-	x_prio = A * x;
-	y = H * x_prio;
-	P = A * P * A' + Q;
-	K = P * H' * inv(H*P*H' +R)
-	x = x_prio + K * (data(i,:)' - y);
-	P = (eye(4) - K * H) * P;
-  size(K)
-  
-  K_data_for_plot(i,1) = K(1,1);
-  K_data_for_plot(i,2) = K(3,1);  
-  
-  
-  P_data_for_plot(i,1) = P(1,1); 
-  P_data_for_plot(i,2) = P(2,2); 
-  P_data_for_plot(i,3) = P(3,3); 
-  P_data_for_plot(i,4) = P(4,4); 
-  
-	data_kalman(i,:) = x;
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %        ADD YOUR CODE HERE          %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    
+    
 end;
 
-hold on;
-figure(1); 
-plot(data_kalman(:,1),data_kalman(:,2), 'r');
-hold off; 
 
-figure(2); 
-plot(data_kalman(:,3)); 
-hold on; 
-plot(data_kalman(:,4));
-hold off;
 
-figure(3); 
-plot(P_data_for_plot(:,1)); 
-hold on; 
-plot(P_data_for_plot(:,2)); 
-plot(P_data_for_plot(:,3)); 
-plot(P_data_for_plot(:,4)); 
-hold off; 
+# TASK 2: Plot the result of the Kalman filter
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%        ADD YOUR CODE HERE          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-figure(4);
-hold on; 
-plot(K_data_for_plot(:,1));  
-plot(K_data_for_plot(:,2));  
+
+# TASK 3: Plot the estimated values for the velocity over time
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%        ADD YOUR CODE HERE          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+# TASK 4: Plot values on the main diagonal in the covariance matrix over time
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%        ADD YOUR CODE HERE          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
